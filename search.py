8c62d6419bda0b3a9a2dfcbfe6a7b4616ce2a3ea
AndreUs-bit <garciaandres2001@gmail.com>
2023-05-26 19:49:37 -0700

    Part 2 of Project
    Added cross validation, currently testing with large input

--- search.py
+++ search.py
@@ -1,7 +1,33 @@
 import random
+import math
 
 def leave_one_out_cross_validation(data, current_set, feature_to_add):
-    return random.randint(0,99)
+    number_correct = 0
+    columns = []
+    for num in current_set:
+        columns.append(num)
+    columns.append(feature_to_add)
+    for i in range(0,len(data.index)):
+        object_to_classify = data.iloc[i,columns]
+        label_object_to_classify = data.iloc[i,0]
+        nearest_neighbor_label = 0
+
+        nearest_neighbor_distance = float("inf")
+        nearest_neighbor_location = float("inf")
+        for k in range(0,len(data.index)):
+            if k != i:
+                comparison = data.iloc[k,columns]
+                sum = 0
+                for l in range(0,len(comparison.index)):
+                    sum += (float(object_to_classify.iloc[l]) - float(comparison.iloc[l]))**2
+                distance = math.sqrt(sum)
+                if distance < nearest_neighbor_distance:
+                    nearest_neighbor_distance = distance
+                    nearest_neighbor_location = k
+                    nearest_neighbor_label = data.iloc[nearest_neighbor_location,0]
+        if label_object_to_classify == nearest_neighbor_label:
+            number_correct += 1
+    return number_correct/data.shape[0]
 
 def feature_search(data):
     current_set_of_features = set()
@@ -13,6 +39,7 @@ def feature_search(data):
             if k not in current_set_of_features:
                 print("--Considering adding the " + str(k) + "th feature")
                 accuracy = leave_one_out_cross_validation(data,current_set_of_features,k)
+                print(accuracy)
                 if accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = accuracy
                     feature_to_add_at_level = k
