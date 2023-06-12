e674e2447dcdb7c345197514573da328ff83392c
AndreUs-bit <garciaandres2001@gmail.com>
2023-06-11 17:27:10 -0700

    Final Version for Submision
    Now includes menu and backward elimination!

--- main.py
+++ main.py
@@ -4,9 +4,32 @@ import math
 import search
 
 def main():
+    print("Welcome to 862173656's Feature Selection Algorithm")
+    file = input("Enter the name of the file to test: ")
 
-    df = pd.read_csv("CS170_Spring_2023_Large_data__73.txt", sep= "\s+", header= None)
-    search.feature_search(df)
+    df = pd.read_csv(file, sep= "\s+", header= None)
+    print("Type the number of the Algorithm you wish to run: ")
+    print("1 - Forward Selection")
+    algorithm = input("2 - Backward Elimination\n")
+    if algorithm == '1':
+        all_features = set()
+        for i in range(1,len(df.columns)):
+            all_features.add(i)
+        print("This data has " + str(len(df.columns) - 1) + " features, with " + str(df.shape[0]) + " instances" )
+        print("Running nearest neighbor with no features (default rate), using \'leave-one-out\' evaluation, I get an accuracy of: " + str(search.leave_one_out_cross_validation(df,all_features, None, "none")))
+        print("Beginning Search: ")
+        search.forward_search(df)
+    elif algorithm == '2':
+        all_features = set()
+        for i in range(1,len(df.columns)):
+            all_features.add(i)
+        print("This data has " + str(len(df.columns) - 1) + " features, with " + str(df.shape[0]) + " instances" )
+        print("Running nearest neighbor with no features (default rate), using \'leave-one-out\' evaluation, I get an accuracy of: " + str(search.leave_one_out_cross_validation(df,all_features, None, "none")))
+        print("Beginning Search: ")
+        search.backward_elimination(df)
+
+
+
 
 if __name__ == "__main__": main()
 
--- search.py
+++ search.py
@@ -1,12 +1,13 @@
 import random
 import math
 
-def leave_one_out_cross_validation(data, current_set, feature_to_add):
+def leave_one_out_cross_validation(data, current_set, feature_to_add, type_of_search):
     number_correct = 0
     columns = []
     for num in current_set:
         columns.append(num)
-    columns.append(feature_to_add)
+    if type_of_search == 'forward':
+        columns.append(feature_to_add)
     for i in range(0,len(data.index)):
         object_to_classify = data.iloc[i,columns]
         label_object_to_classify = data.iloc[i,0]
@@ -29,10 +30,11 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
             number_correct += 1
     return number_correct/data.shape[0]
 
-def feature_search(data):
+def forward_search(data):
     current_set_of_features = set()
     for i in range(1, len(data.columns)):
         print("On the " + str(i) + "th level of the search tree" )
+        print("Current set: ", current_set_of_features)
         feature_to_add_at_level = []
         if i == 1:
             best_so_far_accuracy = 0
@@ -40,8 +42,8 @@ def feature_search(data):
         for k in range(1, len(data.columns)):
             if k not in current_set_of_features:
                 print("--Considering adding the " + str(k) + "th feature")
-                accuracy = leave_one_out_cross_validation(data,current_set_of_features,k)
-                print(accuracy)
+                accuracy = leave_one_out_cross_validation(data,current_set_of_features,k, 'forward')
+                print("Accuracy: " + str(accuracy))
                 if accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = accuracy
                     feature_to_add_at_level = k
@@ -50,5 +52,37 @@ def feature_search(data):
             break
         current_set_of_features.add(feature_to_add_at_level)
         print("On level " + str(i) + " I added feature " + str(feature_to_add_at_level) + " to current set")
+        print("Current set:", current_set_of_features, "accuracy: " + str(best_so_far_accuracy))
+    print('Best Set of Features: ', current_set_of_features)
+    print("With Accuracy: " + str(best_so_far_accuracy))
+
+
+def backward_elimination(data):
+    current_set_of_features = set()
+    for i in range(1,len(data.columns)):
+        current_set_of_features.add(i)
+    for i in range(1, len(data.columns)):
+        print("On the " + str(i) + "th level of the search tree" )
+        print("Current Set of Features: ", current_set_of_features)
+        feature_to_remove_at_level = []
+        if i == 1:
+            best_so_far_accuracy = 0
+        removed_from_set = False
+        for k in range(1, len(data.columns)):
+            if k in current_set_of_features:
+                print("--Considering leaving out the " + str(k) + "th feature")
+                current_set_of_features.remove(k)
+                accuracy = leave_one_out_cross_validation(data,current_set_of_features,k, 'backward')
+                print("Accuracy: " + str(accuracy))
+                current_set_of_features.add(k)
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_remove_at_level = k
+                    removed_from_set = True
+                    current_set_of_features.add(k)
+        if removed_from_set == False:
+            break
+        current_set_of_features.remove(feature_to_remove_at_level)
+        print("On level " + str(i) + " I removed feature " + str(feature_to_remove_at_level) + " from current set")
     print('Best Set of Features: ', current_set_of_features)
     print("With Accuracy: " + str(best_so_far_accuracy))
